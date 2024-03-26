61d4179db3d14949b6f1689305e0c1b1e43357e9
OrhanDg <107655312+OrhanDg@users.noreply.github.com>
2024-03-26 17:10:28 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+function topla() {
+    var sayi1 = document.getElementById('Number 1').value;
+    var sayi2 = document.getElementById('Number 2').value;
+    var sonuc = parseFloat(Number 1) + parseFloat(Number 2);
+    document.getElementById('sonuc').innerHTML = 'Sonuç: ' + sonuc;
+}
